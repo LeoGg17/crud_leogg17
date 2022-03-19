3c2d52900f350c698b7b660d72bcd480dbe56ff0
LeoGg17 <leoguartambel777@gmail.com>
2022-03-18 21:48:43 -0500

    crud

---
+++ src/main/java/com/leo/crud/repositories/IProductDAO.java
@@ -0,0 +1,12 @@
+package com.leo.crud.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leo.crud.models.ProductDTO;
+
+@Repository
+public interface IProductDAO extends MongoRepository<ProductDTO, String> {
+	
+	
+}
